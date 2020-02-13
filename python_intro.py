5da50cd7be00cd284cd80d091e583aca8f96e7f5
David South <terpsfreak@gmail.com>
2020-02-12 19:09:38 -0500

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,11 @@
+def hi(name):
+    print('Hi ' + name + '!')
+
+
+family = ['Alex', 'David', 'Thomas', 'Kirsten']
+for name in family:
+    hi(name)
+    print('Next family member')
+
+for i in range(1, 6):
+    print(i)
